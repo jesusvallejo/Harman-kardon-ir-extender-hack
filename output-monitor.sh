df75e9e051de1b6357789069df4b27361e565a7e
Jesus Vallejo <jesus.vallejo.collados@gmail.com>
2019-02-15 21:17:37 +0100

    special command added

--- output-monitor.sh
+++ output-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR='/proc/asound/card0/pcm0p/sub0/status'
-CMD1='wget --delete-after http://192.168.1.72/VCR1/'
+CMD1='wget --delete-after http://192.168.1.72/special/'
 CMD='wget --delete-after http://192.168.1.72/VCR2/'
 
 
