4539a4b04b997d3b94429e5e21a630b96127ee46
Jesus Vallejo <jesus.vallejo.collados@gmail.com>
2020-05-31 13:55:11 +0200

    Update espIR.ino

--- espIR.ino
+++ espIR.ino
@@ -50,14 +50,14 @@ bool connectToMqtt= true;
 int counter=0;
 // Dont let mqtt hijak service
 // Replace with your network credentials
-const char* ssid     = "keepAway";
-const char* password = "cometa1997";
+const char* ssid     = "XXXXXXX";
+const char* password = "XXXXXXX";
 
 // Replace with your mqtt server credentials
 const char* mqttServer = "192.168.1.7";
 const int mqttPort = 1883;
-const char* mqttUser = "jesus";
-const char* mqttPassword = "cometa1997";
+const char* mqttUser = "XXXXXX";
+const char* mqttPassword = "XXXXXXX";
 
 // Set web server port number to 80
 WiFiServer server(80);
